5b7034a128ba0d0a87a37c83073597657ac722c8
Papp David <pappdav@gmail.com>
2016-06-07 17:59:11 +0200

    Integrate codeship build && Selenium hub url support for environment variable

--- join-hub.sh
+++ join-hub.sh
@@ -1,1 +1,8 @@
-/usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 ${PHANTOMJS_OPTS} --webdriver-selenium-grid-hub http://hub:4444
+#!/bin/bash
+
+if [ -z "$SELENIUM_HUB" ]; then
+  echo "Missing \$SELENIUM_HUB"
+  exit 1
+fi
+
+/usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 ${PHANTOMJS_OPTS} --webdriver-selenium-grid-hub http://${SELENIUM_HUB}:4444
